6be4e054a1ceee5ac1791533486ac377be12626c
ka Kotey <newpontos@gmail.com>
2024-10-26 20:42:54 +0300

    fuck it 3

--- src/physics/physics.h
+++ src/physics/physics.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "bounding_box.h"
-//#include "quadtree.h"
-#include "loose_octree.h"
+#include "quadtree.h"
+//#include "loose_octree.h"
 #include "physics_core.h"
 #include "physics_system.h"
