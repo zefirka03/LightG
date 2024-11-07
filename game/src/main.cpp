2f5822f3febaf625d1092748d59d897355e41608
zefirka03 <newpontos@gmail.com>
2024-11-07 11:31:16 +0300

    fix cmake

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,7 +1,7 @@
 #include "scenes/scene_rtx_test.h"
 
 int main() {
-    Application game(1280, 720, "Light");
+    Application game(1280, 720, "Fuck my ass");
     game.run(new SceneRtxTest());
 
     return 0;
