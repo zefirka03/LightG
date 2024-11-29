70357f34cbcf93f3535f2868fe176e4404b9dccd
ka Kotey <newpontos@gmail.com>
2024-11-29 23:20:41 +0300

    device

--- air_engine/src/core/vk/vulkan_device.cpp
+++ air_engine/src/core/vk/vulkan_device.cpp
@@ -1,7 +1,8 @@
 #include "vulkan_device.h"
 
-VulkanDevice::VulkanDevice(Application &application){
-
+VulkanDevice::VulkanDevice(Application& application) 
+	: m_window(application) {
+	
 }
 
 VulkanDevice::~VulkanDevice(){
--- air_engine/src/core/vk/vulkan_device.h
+++ air_engine/src/core/vk/vulkan_device.h
@@ -2,6 +2,19 @@
 #include "../application.h"
 
 class VulkanDevice {
+private:
+    VkInstance m_instance;
+    VkDebugUtilsMessengerEXT m_debug_messenger;
+    VkPhysicalDevice m_physical_device = VK_NULL_HANDLE;
+    Application &m_window;
+    VkCommandPool m_command_pool;
+    VkDevice m_device;
+
+    VkSurfaceKHR m_surface;
+    VkQueue m_graphicsQueue;
+    VkQueue m_presentQueue;
+
+public:
     VulkanDevice(Application &application);
     ~VulkanDevice();
 
